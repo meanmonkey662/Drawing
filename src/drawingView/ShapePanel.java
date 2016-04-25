b3d9966b72047e5177a55163b50d9f58bce41f40
meanmonkey662 <coolkoch8535@gmail.com>
2016-04-25 12:08:54 -0600

    shape panel

--- src/drawingView/ShapePanel.java
+++ src/drawingView/ShapePanel.java
@@ -1,11 +1,16 @@
 package drawingView;
 
+import javax.swing.JPanel;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
 
 public class ShapePanel extends JPanel
 {
 	private ArrayList<Rectangle> rectangleList;
 	private ArrayList<Ellipse2D> ellipseList;
-	private ArrayList<Ellise2D> circleList;
+	private ArrayList<Ellipse2D> circleList;
 	private ArrayList<Polygon> triangleList;
 	private ArrayList<Polygon> polygonList;
 	
